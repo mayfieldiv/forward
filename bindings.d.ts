22de0552638acfdb0ad04d16d97fd3c87a02bee1
Mayfield <3902405+mayfieldiv@users.noreply.github.com>
2021-09-26 16:42:56 -0400

    forward emails (no attachments)

---
+++ bindings.d.ts
@@ -0,0 +1,9 @@
+export {};
+
+declare global {
+  const EMAIL_DOMAIN: string;
+  const FORWARD_TO_EMAIL_ADDRESS: string;
+  const SENDGRID_API_KEY: string;
+  const SENTRY_DSN: string;
+  const WEBHOOK_SECRET: string;
+}
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+import Toucan from "toucan-js";
+
+addEventListener("fetch", (event: FetchEvent): void => {
+  const sentry = initSentry(event);
+  event.respondWith(
+    handleRequest(event.request).catch((err) => {
+      sentry.captureException(err);
+      return new Response(err.stack, { status: 500 });
+    })
+  );
+});
+
+async function handleRequest(request: Request): Promise<Response> {
+  if (!request.url.endsWith(WEBHOOK_SECRET)) {
+    throw new Error("Invalid request webhook secret");
+  }
+
+  const formData = await request.formData();
+  const body = Object.fromEntries(formData.entries());
+  console.log("fields: " + Object.keys(body).join(", "));
+  Object.entries(body).forEach(([key, value]) =>
+    console.log(`${key}: ${value}`)
+  );
+
+  await forwardEmail(SENDGRID_API_KEY, FORWARD_TO_EMAIL_ADDRESS, body);
+
+  return new Response();
+}
+
+export const forwardEmail = (
+  apiKey: string,
+  forwardTo: string,
+  originalEmailData: { [key: string]: FormDataEntryValue }
+): Promise<void> =>
+  fetch("https://api.sendgrid.com/v3/mail/send", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      personalizations: [{ to: [{ email: forwardTo }] }],
+      from: {
+        name: `${originalEmailData.from}`,
+        email: `forward@${EMAIL_DOMAIN}`,
+      },
+      subject: originalEmailData.subject,
+      content: [
+        {
+          type: "text/plain",
+          value: originalEmailData.text,
+        },
+        {
+          type: "text/html",
+          value: originalEmailData.html,
+        },
+      ],
+    }),
+  }).then(async (it) => {
+    if (!it.ok) {
+      console.error(await it.json());
+      throw new Error("Error forwarding email");
+    }
+  });
+
+// from https://github.com/cloudflare/worker-sentry/blob/1b0f10005114cf41987043c928507f1adca4f25b/index.js
+function initSentry(event: FetchEvent): Toucan {
+  const request = event.request;
+
+  const sentry = new Toucan({
+    dsn: SENTRY_DSN,
+    event: event,
+    allowedHeaders: [
+      "user-agent",
+      "cf-challenge",
+      "accept-encoding",
+      "accept-language",
+      "cf-ray",
+      "content-length",
+      "content-type",
+      "x-real-ip",
+      "host",
+    ],
+    allowedSearchParams: /(.*)/,
+    rewriteFrames: { root: "/" },
+  });
+
+  const colo = request?.cf.colo || "UNKNOWN";
+  sentry.setTag("colo", colo);
+  sentry.setUser({
+    ip: request.headers.get("cf-connecting-ip"),
+    userAgent: request.headers.get("user-agent") || "",
+    colo,
+  });
+
+  return sentry;
+}
